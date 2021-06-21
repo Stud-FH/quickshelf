c25d5c86fbabdce87d6bd78186a84789c5161283
Florian Herzog <79991647+Stud-FH@users.noreply.github.com>
2021-06-21 15:57:09 +0200

    spring initializr template

---
+++ src/main/java/fh/server/PizzaShopApplication.java
@@ -0,0 +1,13 @@
+package fh.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShopApplication.class, args);
+	}
+
+}
