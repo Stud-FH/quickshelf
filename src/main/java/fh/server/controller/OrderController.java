5c91c935a3c99748248edea61626e4ede1a3a09c
Florian Herzog <79991647+Stud-FH@users.noreply.github.com>
2021-06-23 08:04:43 +0200

    basic order design

--- src/main/java/fh/server/controller/OrderController.java
+++ src/main/java/fh/server/controller/OrderController.java
@@ -1,5 +1,36 @@
 package fh.server.controller;
 
+import fh.server.constant.OrderStatus;
+import fh.server.rest.dto.OrderDTO;
+import fh.server.service.OrderService;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public OrderDTO placeOrder(OrderDTO orderDTO) {
+        // TODO
+        return null;
+    }
+
+    public OrderDTO getOrder(Long id) {
+        // TODO
+        return null;
+    }
+
+    public Collection<OrderDTO> getAllOrders(String token, Optional<OrderStatus> status, Optional<String> accountname) {
+        // TODO
+        return null;
+    }
+
 
 }
--- src/main/java/fh/server/service/OrderService.java
+++ src/main/java/fh/server/service/OrderService.java
@@ -1,4 +1,38 @@
 package fh.server.service;
 
+import fh.server.entity.Order;
+import fh.server.repository.OrderRepository;
+import fh.server.rest.dto.OrderDTO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(
+            @Qualifier("orderRepository") OrderRepository orderRepository
+    ) {
+        this.orderRepository = orderRepository;
+    }
+
+
+
+    public Order placeOrder(Order blueprint) {
+        // TODO
+        return null;
+    }
+
+    public Order getOrder(Long id) {
+        // TODO
+        return null;
+    }
+
+    public Collection<Order> getAllOrders() {
+        // TODO
+        return null;
+    }
 }
